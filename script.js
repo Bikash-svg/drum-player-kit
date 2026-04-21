ab935b4ac49993ef86a3e8c0ce3f3ff7a38810c4
bkas <bikashmurmu7738@gmail.com>
2026-04-21 20:39:19 +0530

    audio play by btn and keyboard logic done

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const btnA = document.querySelector('#a');
-const btnS = document.querySelector('#s');
-const btnD = document.querySelector('#d');
-const btnF = document.querySelector('#f');
-const btnG = document.querySelector('#g');
-const btnH = document.querySelector('#h');
-const btnJ = document.querySelector('#j');
-const btnK = document.querySelector('#k');
-const btnL = document.querySelector('#l');
-
-window.addEventListener("keydown", (e) => {
+const keyboardPlay = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.key}"]`)
-    const keyDownAction = e.key;
-    console.log("audioKey " + audio);
+    // const keyDownAction = e.key;
+    if(!audio) return;
     audio.currentTime = 0;
     audio.play()
-})
+}
 
-window.addEventListener("click", (e) => {
-    const clickedBtn = e.target.id;
-    console.log(clickedBtn);
-})
+const btnPlay = (btn) => {
+    const audio = document.querySelector(`audio[data-key="${btn}"]`)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play()
+}
+
+addEventListener("click", (e) => {
+    const btn = e.target.getAttribute("btn-data");
+    btnPlay(btn);
+});
+
+window.addEventListener("keydown", (e) => {
+    keyboardPlay(e);
+});
